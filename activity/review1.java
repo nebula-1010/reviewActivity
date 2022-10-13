e994cd33ebb6233ee7db4627fc68fb2ae6533a39
AustenLowder2002 <112116586+AustenLowder2002@users.noreply.github.com>
2022-10-13 14:45:02 -0400

    Started arrays and etc

--- activity/review1.java
+++ activity/review1.java
@@ -1,6 +1,16 @@
+
 public class review1 {
     public static void main(String[] args){
-        String Myname = "My name";
+        Scanner input = new Scanner(System.in);
+        int courseAmt = input.nextInt();
+        int courseCredit = 0;
+        String coueseName;
+        int[] credithour = new int[courseAmt];
+        String[] coursename = new String[courseAmt];
+        int[] coursescore = new int[courseAmt];
+        for(int i =1; i > courseAmt; i++) {
+            courseName
 
+        }
     }
 }
