6e39858a6642b1f309acf340fef4ee5fba257433
AustenLowder2002 <112116586+AustenLowder2002@users.noreply.github.com>
2022-10-13 14:57:25 -0400

    Changed some stuff
    Like things

--- activity/review1.java
+++ activity/review1.java
@@ -1,15 +1,16 @@
 
 public class review1 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int courseAmt = input.nextInt();
         int courseCredit = 0;
-        String coueseName;
+        String courseName;
         int[] credithour = new int[courseAmt];
         String[] coursename = new String[courseAmt];
         int[] coursescore = new int[courseAmt];
-        for(int i =1; i > courseAmt; i++) {
-            courseName
+        for (int i = 1; i > courseAmt; i++) {
+            courseName = input.nextLine();
+            courseCredit = Integer.parseInt(String.valueOf(input.nextInt()));
 
         }
     }
